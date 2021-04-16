8a8df9b9f7737423e54981d76174e11c9bbced19
henettaja <henri.vaisane@gmail.com>
2021-04-16 18:53:47 +0300

    Exercise 2.18 done

--- osa2/puhelinluettelo/src/services/personsService.js
+++ osa2/puhelinluettelo/src/services/personsService.js
@@ -3,17 +3,22 @@ const baseUrl = "http://localhost:3001/persons"
 
 const getAll = () => {
 	const request = axios.get(baseUrl)
-	return request.then((response) => response.data)
+	return request.then(response => response.data)
 }
 
-const create = (newPerson) => {
+const create = newPerson => {
 	const request = axios.post(baseUrl, newPerson)
-	return request.then((response) => response.data)
+	return request.then(response => response.data)
 }
 
-const remove = (personId) => {
+const update = (personId, personData) => {
+	const request = axios.put(`${baseUrl}/${personId}`, personData)
+	return request.then(response => response.data)
+}
+
+const remove = personId => {
 	const request = axios.delete(`${baseUrl}/${personId}`)
-	return request.then((response) => response.data)
+	return request.then(response => response.data)
 }
 
-export default { getAll, create, remove }
+export default { getAll, create, update, remove }
