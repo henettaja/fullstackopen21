bd81f5a534f3cb1d7c5de088a199e8f03004dfea
Henri Väisänen <henri.vaisanen@tietoevry.com>
2022-12-29 16:02:48 +0200

    Return empty div if no status message

--- osa2/puhelinluettelo/src/components/StatusText.js
+++ osa2/puhelinluettelo/src/components/StatusText.js
@@ -1,11 +1,14 @@
 import React from "react"
 
 const StatusText = ({ status }) => {
-	if (status.err) {
-		return <div className={"error"}>{status.msg}</div>
-	} else {
-		return <div className="status">{status.msg}</div>
+	if (status.msg) {
+		if (status.err) {
+			return <div className={"error"}>{status.msg}</div>
+		} else {
+			return <div className="status">{status.msg}</div>
+		}
 	}
+	return <div></div>
 }
 
 export default StatusText
